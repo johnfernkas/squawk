87aa7ea523e09e8fcf918225cd93ee26866f829e
John Fernkas <466754+johnfernkas@users.noreply.github.com>
2026-04-25 19:34:33 -0400

    Initial Commit

---
+++ squawk/squawkApp.swift
@@ -0,0 +1,17 @@
+//
+//  squawkApp.swift
+//  squawk
+//
+//  Created by John Fernkas on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct squawkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
